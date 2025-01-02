f329d2d5a5af953131f8af999b01a39678a5771c
hoshikira18 <khuyen.dev183@gmail.com>
2025-01-02 16:27:49 +0700

    feat: industry api, industry ui

--- src/components/sections/home/uses.js
+++ src/components/sections/home/uses.js
@@ -1,35 +1,26 @@
-import { getProductCategories } from '@/api/product-categories/api';
 import Button from '@/components/common/button';
 import Title from '@/components/common/title';
 import UsesCard from '@/components/homepage/uses/uses-card';
-import UsesCardSkeleton from '@/components/homepage/uses/uses-card-skeleton';
+import { getIndustryUses } from '@/api/industry-uses/api';
 
 const Uses = async () => {
-    const product_categories = await getProductCategories();
+    const industryUses = await getIndustryUses();
     return (
         <div className="bg-primary px-5 pb-10 shadow-md">
             <Title className="text-center text-white">
-                Ứng dụng ngành công nghiệp
+                Ứng dụng ngành
             </Title>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                {product_categories?.map((category, index) => {
+                {industryUses?.map((use, index) => {
                     return (
                         <UsesCard
-                            key={category?.id}
-                            categoryId={category?.id}
-                            categoryTitle={category?.display_name}
-                            categoryImage={category?.metadata?.image}
+                            key={use?.id}
+                            useId={use?.id}
+                            useImage={use?.image_url}
+                            useName={use?.name}
                         />
                     );
                 })}
-            </div>
-            <div className="mt-10 flex justify-center">
-                <Button
-                    href="/"
-                    className="bg-white text-primary hover:border-primary"
-                >
-                    Xem thêm
-                </Button>
             </div>
         </div>
     );
